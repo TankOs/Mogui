d2e04aee4c079d2ed416d9318e8d9e8b67429d3f
Stefan Schindler <stefan@boxbox.org>
2014-03-10 00:07:47 +0100

    Removed obsolete renderer code. Renderer inherits from DrawableContainer.

--- lib/include/mogui/opengl_es_renderer.hpp
+++ lib/include/mogui/opengl_es_renderer.hpp
@@ -8,22 +8,11 @@ namespace mog {
 
 class OpenGLESRenderer : public Renderer {
 	public:
-		void drawable_count( std::size_t count ) override;
-		std::size_t drawable_count() const override;
-
-		mog::Color drawable_color( std::size_t index ) override;
-		void drawable_color( std::size_t index, mog::Color color ) override;
-
 	private:
-		struct Drawable {
-			mog::Color color;
-		};
-
-		std::vector<Drawable> _drawables;
 };
 
 }
 
 /** @class mog::OpenGLESRenderer
- * Mogui renderer that's compatible to OpenGL ES 2.
+ * Renderer that's compatible to OpenGL ES 2.
  */
--- lib/src/mogui/opengl_es_renderer.cpp
+++ lib/src/mogui/opengl_es_renderer.cpp
@@ -3,22 +3,4 @@
 
 namespace mog {
 
-void OpenGLESRenderer::drawable_count( std::size_t count ) {
-	_drawables.resize( count );
-}
-
-std::size_t OpenGLESRenderer::drawable_count() const {
-	return _drawables.size();
-}
-
-mog::Color OpenGLESRenderer::drawable_color( std::size_t index ) {
-	assert( "Invalid drawable index." && index < _drawables.size() );
-	return _drawables[index].color;
-}
-
-void OpenGLESRenderer::drawable_color( std::size_t index, mog::Color color ) {
-	assert( "Invalid drawable index." && index < _drawables.size() );
-	_drawables[index].color = color;
-}
-
 }
